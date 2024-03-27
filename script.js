2c9efa36cfb223c4a877d83be5cc894378d51090
Ayush Kashyap <ayushkashyap0507@gmail.com>
2024-03-27 14:19:57 +0530

    added new projects

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ setInterval(() => {
     document.querySelector('.change').innerHTML=changable[i%2]
     i++;
 }, 2000);
-window.addEventListener("scroll",callProject);
-
-function callProject(){
-    window.location.assign("/projects.html")
-}
